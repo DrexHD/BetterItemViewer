d5173b590903c975c61b3dfefcc69c5534806703
Drex <nicknamedrex@gmail.com>
2026-02-02 14:22:14 +0100

    Improve mob drop initialization logic

--- src/main/java/me/drex/betteritemviewer/item/ItemManager.java
+++ src/main/java/me/drex/betteritemviewer/item/ItemManager.java
@@ -157,57 +157,58 @@ public class ItemManager {
     public void findMobDrops() {
         World defaultWorld = Universe.get().getDefaultWorld();
         if (defaultWorld == null) return;
+        defaultWorld.execute(() -> {
+            NPCPlugin npcPlugin = NPCPlugin.get();
+            List<String> roles = npcPlugin.getRoleTemplateNames(true);
+            TransformComponent transformComponent = new TransformComponent();
 
-        NPCPlugin npcPlugin = NPCPlugin.get();
-        List<String> roles = npcPlugin.getRoleTemplateNames(true);
-        TransformComponent transformComponent = new TransformComponent();
+            Vector3d pos = new Vector3d(transformComponent.getPosition());
+            String name = roles.getFirst();
+            int roleIndex = NPCPlugin.get().getIndex(name);
+            if (roleIndex < 0) {
+                throw new IllegalStateException("No such valid role: " + name);
+            } else {
+                Pair<Ref<EntityStore>, NPCEntity> npcPair = npcPlugin.spawnEntity(defaultWorld.getEntityStore().getStore(), roleIndex, pos, null, null, null);
+                NPCEntity npcComponent = npcPair.second();
 
-        Vector3d pos = new Vector3d(transformComponent.getPosition());
-        String name = roles.getFirst();
-        int roleIndex = NPCPlugin.get().getIndex(name);
-        if (roleIndex < 0) {
-            throw new IllegalStateException("No such valid role: " + name);
-        } else {
-            Pair<Ref<EntityStore>, NPCEntity> npcPair = npcPlugin.spawnEntity(defaultWorld.getEntityStore().getStore(), roleIndex, pos, null, null, null);
-            NPCEntity npcComponent = npcPair.second();
+                for (String roleName : roles) {
+                    try {
+                        BuilderInfo builderInfo = NPCPlugin.get().prepareRoleBuilderInfo(NPCPlugin.get().getIndex(roleName));
+                        Builder<Role> roleBuilder = (Builder<Role>) builderInfo.getBuilder();
+                        Holder<EntityStore> holder = EntityStore.REGISTRY.newHolder();
+                        holder.addComponent(NPCEntity.getComponentType(), npcComponent);
+                        BuilderSupport builderSupport = new BuilderSupport(
+                            NPCPlugin.get().getBuilderManager(), npcComponent, holder, new ExecutionContext(), roleBuilder, null
+                        );
+                        Role role = NPCPlugin.buildRole(roleBuilder, builderInfo, builderSupport, roleIndex);
+                        String memoryName = role.getAppearanceName();
+                        String memoriesNameOverride = getMemoriesNameOverride(roleBuilder);
+                        if (memoriesNameOverride != null && !memoriesNameOverride.isEmpty()) {
+                            memoryName = memoriesNameOverride;
+                        }
+                        PositionCacheSystems.initialisePositionCache(role, builderSupport.getStateEvaluator(), 0.0);
+                        String dropListId = role.getDropListId();
+                        if (dropListId == null) continue;
+                        ItemDropList itemDropList = ItemDropList.getAssetMap().getAsset(dropListId);
+                        if (itemDropList == null) continue;
+                        var drops = new LinkedList<ItemDrop>();
+                        ItemDropContainer container = itemDropList.getContainer();
+                        if (container == null) continue;
+                        container.getAllDrops(drops);
 
-            for (String roleName : roles) {
-                try {
-                    BuilderInfo builderInfo = NPCPlugin.get().prepareRoleBuilderInfo(NPCPlugin.get().getIndex(roleName));
-                    Builder<Role> roleBuilder = (Builder<Role>) builderInfo.getBuilder();
-                    Holder<EntityStore> holder = EntityStore.REGISTRY.newHolder();
-                    holder.addComponent(NPCEntity.getComponentType(), npcComponent);
-                    BuilderSupport builderSupport = new BuilderSupport(
-                        NPCPlugin.get().getBuilderManager(), npcComponent, holder, new ExecutionContext(), roleBuilder, null
-                    );
-                    Role role = NPCPlugin.buildRole(roleBuilder, builderInfo, builderSupport, roleIndex);
-                    String memoryName = role.getAppearanceName();
-                    String memoriesNameOverride = getMemoriesNameOverride(roleBuilder);
-                    if (memoriesNameOverride != null && !memoriesNameOverride.isEmpty()) {
-                        memoryName = memoriesNameOverride;
+                        for (ItemDrop drop : drops) {
+                            String itemId = drop.getItemId();
+                            ItemDetails itemDetails = getOrCreateDetails(itemId);
+                            itemDetails.mobLoot.put(memoryName, new MobDropDetails(role.getNameTranslationKey(), new Range(drop.getQuantityMin(), drop.getQuantityMax())));
+                        }
+                    } catch (Throwable t) {
+                        npcPlugin.getLogger().at(Level.WARNING).log("Error spawning role " + roleName + ": " + t.getMessage());
                     }
-                    PositionCacheSystems.initialisePositionCache(role, builderSupport.getStateEvaluator(), 0.0);
-                    String dropListId = role.getDropListId();
-                    if (dropListId == null) continue;
-                    ItemDropList itemDropList = ItemDropList.getAssetMap().getAsset(dropListId);
-                    if (itemDropList == null) continue;
-                    var drops = new LinkedList<ItemDrop>();
-                    ItemDropContainer container = itemDropList.getContainer();
-                    if (container == null) continue;
-                    container.getAllDrops(drops);
-
-                    for (ItemDrop drop : drops) {
-                        String itemId = drop.getItemId();
-                        ItemDetails itemDetails = getOrCreateDetails(itemId);
-                        itemDetails.mobLoot.put(memoryName, new MobDropDetails(role.getNameTranslationKey(), new Range(drop.getQuantityMin(), drop.getQuantityMax())));
-                    }
-                } catch (Throwable t) {
-                    npcPlugin.getLogger().at(Level.WARNING).log("Error spawning role " + roleName + ": " + t.getMessage());
                 }
-            }
 
-            npcComponent.remove();
-        }
+                npcComponent.remove();
+            }
+        });
     }
 
     // Copied from NPCMemoryProvider.getMemoriesNameOverride
